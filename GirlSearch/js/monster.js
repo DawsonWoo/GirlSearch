13a98806d8a4849e39935ff29405c594c231b6c6
DawsonWoo <woodc123@163.com>
2022-10-30 14:52:28 +0800

    Add files via upload

---
+++ GirlSearch/js/monster.js
@@ -0,0 +1,50 @@
+var monster1 = new Image();
+monster1.src = "pic/mon - 1.jpg";
+var monster2 = new Image();
+monster2.src = "pic/mon - 2.jpg";
+var monster3 = new Image();
+monster3.src = "pic/mon - 3.jpg";
+var monster4 = new Image();
+monster4.src = "pic/mon - 4.jpg";
+var monster5 = new Image();
+monster5.src = "pic/mon - 5.jpg";
+var monster6 = new Image();
+monster6.src = "pic/mon - 6.jpg";
+var monster7 = new Image();
+monster7.src = "pic/mon - 7.jpg";
+var monster8 = new Image();
+monster8.src = "pic/mon - 8.jpg";
+var monster9 = new Image();
+monster9.src = "pic/mon - 9.jpg";
+var monster10 = new Image();
+monster10.src = "pic/mon - 10.jpg";
+var monster11 = new Image();
+monster11.src = "pic/mon - 11.jpg";
+var monster12 = new Image();
+monster12.src = "pic/mon - 12.jpg";
+var monsterImgArray = [
+    monster1,
+    monster2,
+    monster3,
+    monster4,
+    monster5,
+    monster6,
+    monster7,
+    monster8,
+    monster9,
+    monster10,
+    monster11,
+    monster12
+];
+var chengdiao = new Image();
+chengdiao.src = "pic/diaomin - 1.jpg";
+var jindiao = new Image();
+jindiao.src = "pic/diaomin - 2.jpg";
+var yangdiao = new Image();
+yangdiao.src = "pic/diaomin - 3.jpg";
+var zhangdiao = new Image();
+zhangdiao.src = "pic/diaomin - 4.jpg";
+var myself = new Image();
+myself.src = "pic/me.jpg";
+
+
